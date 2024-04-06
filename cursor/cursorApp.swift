adf8d129b1fb907f3d0c47a147b6b1360fcd36be
zincLcs96 <12760908+zdirnecamlcs96@users.noreply.github.com>
2024-04-06 12:39:45 +0800

    Initial Commit

---
+++ cursor/cursorApp.swift
@@ -0,0 +1,17 @@
+//
+//  cursorApp.swift
+//  cursor
+//
+//  Created by Ling Chia Sheng on 06/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct cursorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
